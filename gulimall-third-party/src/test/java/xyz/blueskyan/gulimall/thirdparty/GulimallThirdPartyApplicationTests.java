cbf82086349933f2346a3391d393925e75d35e55
BlueSkYan <65001630+BlueSkYan@users.noreply.github.com>
2022-09-21 14:37:34 +0800

    code submit

---
+++ gulimall-third-party/src/test/java/xyz/blueskyan/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.blueskyan.gulimall.thirdparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
